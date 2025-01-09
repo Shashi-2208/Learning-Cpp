758a58aef7375d325853fb4085abc5f6dfc04193
Shashi <shashiranjan22804@gmail.com>
2025-01-09 23:48:18 +0530

    by function

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+/*int main(int argc, char const *argv[])
 {
     int i;
     for ( i = 0; i < 4; i++)
@@ -14,4 +14,20 @@ int main(int argc, char const *argv[])
     }
     
     return 0;
+}*/
+void pattern(int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        for ( int j = 0; j < n; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern(n);
 }
