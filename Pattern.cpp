5e261468c9bedcfd12bbe677e3c1d95d4db0784f
Shashi <shashiranjan22804@gmail.com>
2025-01-09 23:41:28 +0530

    pattern

---
+++ Pattern.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    for ( i = 0; i < 4; i++)
+    {
+        for ( int j = 0; j < 4; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
